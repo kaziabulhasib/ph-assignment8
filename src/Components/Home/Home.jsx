a7004623c33ad35530aa559a2357dba839ce7659
kaziabulhasib <abulhasibkazi@gmail.com>
2024-03-25 19:24:14 +0530

    daisy setup and nested routes check

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-4xl text-center'>This is home.</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,27 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root";
+import Home from "./Components/Home/Home";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/listedbooks",
+        element: (
+          <div>
+            <h1 className='text-5xl text-center'>This is listed books.</h1>
+          </div>
+        ),
+      },
+    ],
   },
 ]);
 
